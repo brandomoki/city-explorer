7203de7556d75531dc0d95edfe07b3799dd3d340
Brandon Pitts <brandomoki.bp@gmail.com>
2022-08-22 23:21:18 -0700

    created form and called api, sucessfully set state with api array

--- src/App.js
+++ src/App.js
@@ -1,32 +1,85 @@
 import React from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
-class App extends React.Component () {
+
+class App extends React.Component {
+
   constructor(props) {
     super(props);
-    locationApiData: []
+    this.state = {
+      cityData: [],
+      city: '',
 
+
+    }
+   
   }
 
 
-handleGetLocationApi = async (e) => {
+
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+getCityData = async (e) => {
   e.preventDefault();
+  let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
 
-  let locationData = await axios.get('https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=Empire%20State%20Building&format=json'
-  )
+  let cityData = await axios.get(url);
+  
+
+  this.setState({
+    cityData: cityData,
+  })
+  console.log(cityData.data[0].lat);
 }
 
-}
 
 render() {
+
+
+
   return (
     <>
-    <h1>HelloWorld</h1>
+    <form onSubmit={this.getCityData}>
+      <label>pick a city
+      <input type='text' onInput={this.handleInput}/>
+
+      </label>
+      
+      <button type='submit' >Explore</button>
+    </form>
+
+    <Card style={{ width: '18rem' }}>
+      <ListGroup variant="flush">
+        <ListGroup.Item>{}</ListGroup.Item>
+        <ListGroup.Item>{}</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card>
     
+
+
     </>
+    
   );
+
 }
+
+
+
+
+
+}
+
+
+
 
 
 
